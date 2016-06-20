e831b4a18e26a257dbd465b90da6f1f85225c6c8
xufang <897445793@qq.com>
2016-06-20 22:47:53 +0800

    optimize FlipperBanner

--- app/src/main/java/com/fangxu/dota2helper/ui/widget/FlipperBanner.java
+++ app/src/main/java/com/fangxu/dota2helper/ui/widget/FlipperBanner.java
@@ -3,6 +3,7 @@ package com.fangxu.dota2helper.ui.widget;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -25,7 +26,7 @@ import java.util.List;
 /**
  * Created by xf on 2015/8/20.
  */
-public class FlipperBanner extends FrameLayout implements  View.OnClickListener, View.OnTouchListener{
+public class FlipperBanner extends FrameLayout implements View.OnClickListener, View.OnTouchListener {
 
     private Context mContext;
     private LinearLayout mDotContainer;
@@ -61,10 +62,10 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
             mViewFlipper.showNext();
             mLastIndex = mCurIndex;
             mCurIndex++;
-            if (mCurIndex == mBannerCount){
+            if (mCurIndex == mBannerCount) {
                 mCurIndex = 0;
             }
-            if (mLastIndex == mBannerCount){
+            if (mLastIndex == mBannerCount) {
                 mLastIndex = 0;
             }
             setSelectedIndicator();
@@ -81,13 +82,13 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
         init(context, attrs);
     }
 
-    private void init(Context context, AttributeSet attrs){
+    private void init(Context context, AttributeSet attrs) {
         mContext = context;
         View view = LayoutInflater.from(mContext).inflate(R.layout.layout_flipper_banner, this);
-        mViewFlipper = (ViewFlipper)view.findViewById(R.id.vf_banner);
-        mDotContainer = (LinearLayout)view.findViewById(R.id.ll_dot_container);
-        LinearLayout bottomContainer = (LinearLayout)view.findViewById(R.id.ll_bottom_container);
-        mTitleTextView = (TextView)view.findViewById(R.id.tv_title);
+        mViewFlipper = (ViewFlipper) view.findViewById(R.id.vf_banner);
+        mDotContainer = (LinearLayout) view.findViewById(R.id.ll_dot_container);
+        LinearLayout bottomContainer = (LinearLayout) view.findViewById(R.id.ll_bottom_container);
+        mTitleTextView = (TextView) view.findViewById(R.id.tv_title);
 
         setOnTouchListener(this);
         setOnClickListener(this);
@@ -109,7 +110,7 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        switch (event.getAction()){
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mStartPressTime = System.currentTimeMillis();
                 stopAutoSwitch();
@@ -122,7 +123,7 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
                 curY = event.getY();
                 absDx = Math.abs(curX - startX);
                 absDy = Math.abs(curY - startY);
-                if (absDx < absDy){
+                if (absDx < absDy) {
                     getParent().requestDisallowInterceptTouchEvent(false);
                 }
                 break;
@@ -130,31 +131,31 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
                 mEndPressTime = System.currentTimeMillis();
                 startAutoSwitch();
                 endX = event.getX();
-                if (endX - startX > 100){//right
+                if (endX - startX > 100) {//right
                     mViewFlipper.setInAnimation(mContext, R.anim.in_left_right);
                     mViewFlipper.setOutAnimation(mContext, R.anim.out_left_right);
                     mViewFlipper.showPrevious();
                     mLastIndex = mCurIndex;
                     mCurIndex--;
-                    if (mCurIndex == -1){
+                    if (mCurIndex == -1) {
                         mCurIndex = mBannerCount - 1;
                     }
                     setSelectedIndicator();
-                } else if (endX - startX< -100){//left
+                } else if (endX - startX < -100) {//left
                     mViewFlipper.setInAnimation(mContext, R.anim.in_right_left);
                     mViewFlipper.setOutAnimation(mContext, R.anim.out_right_left);
                     mViewFlipper.showNext();
                     mLastIndex = mCurIndex;
                     mCurIndex++;
-                    if (mCurIndex == mBannerCount){
+                    if (mCurIndex == mBannerCount) {
                         mCurIndex = 0;
-                }
+                    }
                     setSelectedIndicator();
                 }
                 break;
         }
 
-        if (mEndPressTime - mStartPressTime > 1000 || Math.abs(endX - startX) > 100){
+        if (mEndPressTime - mStartPressTime > 1000 || Math.abs(endX - startX) > 100) {
             return true;
         } else {
             return false;
@@ -168,28 +169,30 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
     }
 
     public void setBanner(List<NewsList.BannerEntity> bannerEntityList) {
-        mBannerEntityList = bannerEntityList;
-        mViewFlipper.removeAllViews();
-        int size = bannerEntityList.size();
-        for (int i = 0; i < size; i++) {
-            ImageView imageView = new ImageView(mContext);
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            mViewFlipper.addView(imageView);
-            if (mShowTitle) {
-                mTitleTextView.setText(bannerEntityList.get(i).getTitle());
+        if (bannerEntityList.size() != mBannerEntityList.size()) {
+            mViewFlipper.removeAllViews();
+            int size = bannerEntityList.size();
+            for (int i = 0; i < size; i++) {
+                ImageView imageView = new ImageView(mContext);
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                mViewFlipper.addView(imageView);
             }
-            Glide.with(mContext).load(bannerEntityList.get(i).getBackground()).placeholder(R.drawable.image_background_default).into(imageView);
+            mLastIndex = 0;
+            mCurIndex = 0;
         }
+        mBannerEntityList = bannerEntityList;
         mBannerCount = mViewFlipper.getChildCount();
-        mLastIndex = 0;
-        mCurIndex = 0;
         addDots();
     }
 
-    private void addDots(){
-        mDotContainer.removeAllViews();
-        if (mShowDot) {
-            for (int i = 0; i < mBannerCount; i++){
+    private void addDots() {
+        if (!mShowDot) {
+            return;
+        }
+        int dotCount = mDotContainer.getChildCount();
+        if (dotCount != mBannerCount) {
+            mDotContainer.removeAllViews();
+            for (int i = 0; i < mBannerCount; i++) {
                 ImageView imageView = new ImageView(mContext);
                 imageView.setImageResource(mDotNormalId);
                 LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(DimenUtil.dp2px(mContext, 5), DimenUtil.dp2px(mContext, 5));
@@ -197,8 +200,13 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
                 imageView.setLayoutParams(lp);
                 mDotContainer.addView(imageView, i);
             }
-            setSelectedIndicator();
+        } else {
+            for (int i = 0; i < mBannerCount; i++) {
+                ImageView imageView = (ImageView)mDotContainer.getChildAt(i);
+                imageView.setImageResource(mDotNormalId);
+            }
         }
+        setSelectedIndicator();
     }
 
     @Override
@@ -213,20 +221,24 @@ public class FlipperBanner extends FrameLayout implements  View.OnClickListener,
         stopAutoSwitch();
     }
 
-    private void stopAutoSwitch(){
+    private void stopAutoSwitch() {
         removeCallbacks(mAutoSwitchRunnable);
     }
 
-    private void startAutoSwitch(){
+    private void startAutoSwitch() {
         removeCallbacks(mAutoSwitchRunnable);
         postDelayed(mAutoSwitchRunnable, 3000);
     }
 
-    private void setSelectedIndicator(){
-        NewsList.BannerEntity bannerEntity = mBannerEntityList.get(mCurIndex);
-        mTitleTextView.setText(bannerEntity.getTitle());
+    private void setSelectedIndicator() {
+        final NewsList.BannerEntity bannerEntity = mBannerEntityList.get(mCurIndex);
+        if (mShowTitle) {
+            mTitleTextView.setText(bannerEntity.getTitle());
+        }
+        final ImageView imageView = (ImageView) mViewFlipper.getChildAt(mCurIndex);
+        Glide.with(mContext).load(bannerEntity.getBackground()).crossFade().placeholder(R.drawable.image_background_default).into(imageView);
 
-        ((ImageView)mDotContainer.getChildAt(mLastIndex)).setImageResource(mDotNormalId);
-        ((ImageView)mDotContainer.getChildAt(mCurIndex)).setImageResource(mDotSelectedId);
+        ((ImageView) mDotContainer.getChildAt(mLastIndex)).setImageResource(mDotNormalId);
+        ((ImageView) mDotContainer.getChildAt(mCurIndex)).setImageResource(mDotSelectedId);
     }
 }
